e76101d59bacf434f256847a16db18a9680c5728
luangabriel <luangabriel-btu17@hotmail.com>
2023-09-25 21:02:21 -0300

    Modificação

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Trilha JS Developer - Pokedex
+
+# Minhas Alterações na Pokedex
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,5 +1,5 @@
 
-const pokeApi = {}
+const pokeAPI = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
@@ -10,26 +10,26 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const [type] = types
 
     pokemon.types = types
-    pokemon.type = type
+    pokemon.type = type 
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+pokeAPI.getPokemonDetail = (pokemon) => {
+   return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeAPI.getPokemons = (offset = 0, limit = 5 ) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
         .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((jsonBody) =>jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeAPI.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+        .then((pokemonsDetails) => pokemonsDetails)    
 }
